7c9ca6f167bb38b0404617bf65a3199599aab7b9
muralimandula <murali.mandula@msitprogram.net>
2019-10-28 18:32:48 +0530

    Day1__Smaple_to_solve_Quiz

---
+++ Day 1 - Intro/Quiz/Runtimes.java
@@ -0,0 +1,23 @@
+
+
+final class RunTimes {
+    public static void main(String[] args) {
+        
+        int s = 0;
+        for (int n = 127; n > 0; n /= 2) {
+
+            s++;
+            for (int i = 0; i < n; i++) {
+
+                // s++;
+            }
+
+        }
+        System.out.println(s);
+    }
+}
+
+// 25  - 47
+// 50  - 97
+// 100 - 197
+// 200 - 397
