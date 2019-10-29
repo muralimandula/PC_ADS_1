ded685bbb5f98602db5140cbdb5ac1318f17a074
muralimandula <murali.mandula@msitprogram.net>
2019-10-29 11:39:49 +0530

    Day1__A1__TripletCount__BinarySeach

--- Day 1 - Intro/A1__Triplet_Sum/UtilityClass.java
+++ Day 1 - Intro/A1__Triplet_Sum/UtilityClass.java
@@ -1,5 +1,33 @@
 import java.util.Arrays;
 
+class BinarySearch {
+
+    public int binarySearch(int[] arr, int l, int r, int x) 
+    { 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+  
+            // If the element is present at the 
+            // middle itself 
+            if (arr[mid] == x) 
+                return mid; 
+  
+            // If element is smaller than mid, then 
+            // it can only be present in left subarray 
+            if (arr[mid] > x) 
+                return binarySearch(arr, l, mid - 1, x); 
+  
+            // Else the element can only be present 
+            // in right subarray 
+            return binarySearch(arr, mid + 1, r, x); 
+        } 
+  
+        // We reach here when element is not present 
+        // in array 
+        return -1; 
+    }
+}
+
 final class UtilityClass {
 
     public static void main(final String[] args) {
@@ -21,7 +49,7 @@ final class UtilityClass {
             }
         }
 
-        System.out.println("Cubic time : " + count + " triplets");
+        System.out.println("Brute force, Cubic : " + count + " triplets");
 
 
         /*
@@ -33,14 +61,14 @@ final class UtilityClass {
 
         count = 0;
 
-        for (int i = 0; i < arr.length - 2; i++) {
+        for (int i = 0; i < arr.length - 2; i++) {   // N
 
             int j = i + 1;            // second element index, index next to first element (Increase towards third)
             int k = arr.length  - 1;  // third element index, index of the last element    (Decrease towards second)
 
             int sum = arr[i] + arr[j] + arr[k];
 
-            while (j < k) {
+            while (j < k) {                            // log N
 
                 // System.out.println( j + " --- " + k);
                 sum = arr[i] + arr[j] + arr[k];
@@ -56,7 +84,39 @@ final class UtilityClass {
             }
 
         }
-        System.out.println("With Sort : " + count + " triplets");
+        System.out.println("With just Sort     : " + count + " triplets");
+
+
+        /**
+         * Now,, Binary Search over the sorted : Searching third element
+         */
+        count = 0;
+        for (int i = 0; i < arr.length; i++) {            // N
+
+            for (int j =  i + 1; j < arr.length; j++) {   // N log N
+
+                int num1 = arr[i];
+                int num2 = arr[j];
+
+                int num3ToSearch = (sumToBe - num1 - num2);
+    
+                int lo = j + 1;
+                int high = arr.length - 1;
+                BinarySearch bs = new BinarySearch();     
+
+                if((num1 < num2) && (num2 < num3ToSearch)) {
+
+                    if (bs.binarySearch(arr, lo, high, num3ToSearch) != -1) {
+                        count++;
+                    }
+                }
+                
+            }
+        }
+        
+        System.out.println("With BinarySearch  : " + count + " triplets");
+
+ 
     }
 
 }
